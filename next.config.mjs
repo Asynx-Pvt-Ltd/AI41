51271779e83e3e581b0f779f00e0eb50a9d7d5a8
Shadowctrl <shadowctrl@outlook.com>
2024-11-03 20:26:34 +0530

    "Updated next.config.mjs, package.json, and tailwind.config.ts files with new configurations and removed unused dependencies."

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,32 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**'
+        hostname: 'enszdm5ppg68ylfd.public.blob.vercel-storage.com',
+        port: ''
+      },
+      {
+        protocol: 'https',
+        hostname: 'qualitrix.com',
+        port: '',
+        pathname: '/wp-content/uploads/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'miro.medium.com',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.allabtai.com',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**'
       }
     ]
   }
