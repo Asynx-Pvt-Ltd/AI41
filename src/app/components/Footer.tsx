e4914cbd12fb950f3c495ffde2d501c30d30bbc0
An-On-Y-mous <kishorerajendran2003@gmail.com>
2024-11-04 23:48:57 +0530

    altered header & footer design

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,49 +1,51 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import "../styles/style.css";
+
 function Footer() {
   return (
-    <footer className='w-full bg-gray-900 text-white py-8 sticky'>
-      <div className='container mx-auto px-4'>
-        <div className='grid grid-cols-1 md:grid-cols-4 gap-8'>
+    <footer className="w-full bg-[#222222] text-white py-8 sticky rounded-t-[3vw] font-roboto text-xl">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Site Branding */}
-          <div className='space-y-4'>
-            <div className='my-2 justify-center site-brand-foot'>
+          <div className="space-y-4">
+            <div className="my-2 justify-center site-brand-foot">
               <Image
-                src='/logo-light-aixploria.webp'
-                alt='Aixploria'
-                title='Aixploria'
-                width='200'
-                height='45'
-                className='logo24'
-                loading='lazy'
-                decoding='async'
+                src="/logo-light-aixploria.webp"
+                alt="Aixploria"
+                title="Aixploria"
+                width="200"
+                height="45"
+                className="logo24"
+                loading="lazy"
+                decoding="async"
               />
             </div>
             <p>Artificial intelligence for everyone</p>
-            <ul className='flex space-x-4'>
+            <ul className="flex space-x-4">
               <li>
-                <a href='#' className='block w-12 h-12'>
+                <a href="#" className="block w-12 h-12">
                   <Image
-                    src='/icon-lang-english-us.webp'
-                    alt='English'
-                    className='mx-auto'
+                    src="/icon-lang-english-us.webp"
+                    alt="English"
+                    className="mx-auto"
                     width={20}
                     height={0}
-                    loading='lazy'
-                    decoding='async'
+                    loading="lazy"
+                    decoding="async"
                   />
                 </a>
               </li>
               <li>
-                <a href='#' className='block w-12 h-12'>
+                <a href="#" className="block w-12 h-12">
                   <Image
-                    src='/icon-lang-fr.webp'
-                    alt='Français'
-                    className='mx-auto'
+                    src="/icon-lang-fr.webp"
+                    alt="Français"
+                    className="mx-auto"
                     width={20}
                     height={0}
-                    loading='lazy'
-                    decoding='async'
+                    loading="lazy"
+                    decoding="async"
                   />
                 </a>
               </li>
@@ -52,20 +54,20 @@ function Footer() {
 
           {/* Resources */}
           <div>
-            <ul className='space-y-2'>
-              <li className='font-bold text-lg'>Resources</li>
+            <ul className="space-y-2">
+              <li className="font-bold text-lg">Resources</li>
               <li>
-                <a href='#' className='hover:text-gray-300'>
+                <a href="#" className="hover:text-gray-300">
                   Tutorials, tips and blog
                 </a>
               </li>
               <li>
-                <a href='#' className='hover:text-gray-300'>
+                <a href="#" className="hover:text-gray-300">
                   AI Conferences Agenda
                 </a>
               </li>
               <li>
-                <a href='#' className='hover:text-gray-300'>
+                <a href="#" className="hover:text-gray-300">
                   AI Glossary & Lexicon
                 </a>
               </li>
@@ -74,25 +76,25 @@ function Footer() {
 
           {/* Useful Tools */}
           <div>
-            <ul className='space-y-2'>
-              <li className='font-bold text-lg'>Useful Tools</li>
+            <ul className="space-y-2">
+              <li className="font-bold text-lg">Useful Tools</li>
               <li>
-                <a href='#' className='hover:text-gray-300'>
+                <a href="#" className="hover:text-gray-300">
                   Best AI Youtube Channels
                 </a>
               </li>
               <li>
-                <a href='#' className='hover:text-gray-300'>
+                <a href="#" className="hover:text-gray-300">
                   Top 100 AI
                 </a>
               </li>
               <li>
-                <a href='#' className='hover:text-gray-300'>
+                <a href="#" className="hover:text-gray-300">
                   GPTs List
                 </a>
               </li>
               <li>
-                <a href='#' className='hover:text-gray-300'>
+                <a href="#" className="hover:text-gray-300">
                   Hubspot AI Tools
                 </a>
               </li>
@@ -101,49 +103,49 @@ function Footer() {
 
           {/* Company */}
           <div>
-            <ul className='space-y-2'>
-              <li className='font-bold text-lg'>Company</li>
+            <ul className="space-y-2">
+              <li className="font-bold text-lg">Company</li>
               <li>
                 <a
-                  href='#'
-                  className='hover:text-gray-300'
-                  rel='nofollow noopener'
+                  href="#"
+                  className="hover:text-gray-300"
+                  rel="nofollow noopener"
                 >
                   Submit an AI Tool
                 </a>
               </li>
               <li>
                 <a
-                  href='#'
-                  className='hover:text-gray-300'
-                  rel='nofollow noopener'
+                  href="#"
+                  className="hover:text-gray-300"
+                  rel="nofollow noopener"
                 >
                   Submit a GPTs
                 </a>
               </li>
               <li>
                 <a
-                  href='#'
-                  className='hover:text-gray-300'
-                  rel='nofollow noopener'
+                  href="#"
+                  className="hover:text-gray-300"
+                  rel="nofollow noopener"
                 >
                   Submit an AI Event
                 </a>
               </li>
               <li>
                 <a
-                  href='#'
-                  className='hover:text-gray-300'
-                  rel='nofollow noopener'
+                  href="#"
+                  className="hover:text-gray-300"
+                  rel="nofollow noopener"
                 >
                   Contact Us
                 </a>
               </li>
               <li>
                 <a
-                  href='#'
-                  className='hover:text-gray-300'
-                  rel='nofollow noopener'
+                  href="#"
+                  className="hover:text-gray-300"
+                  rel="nofollow noopener"
                 >
                   Feature your tool ★
                 </a>
@@ -152,61 +154,61 @@ function Footer() {
           </div>
         </div>
 
-        <div className='border-t border-gray-700 mt-8 pt-6'>
-          <div className='flex flex-col md:flex-row justify-between items-center'>
-            <div className='flex flex-row justify-evenly items-center text-center'>
-              <span className='text-sm'>
+        <div className="border-t border-gray-700 mt-8 pt-6">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="flex flex-row justify-evenly items-center text-center">
+              <span className="text-sm">
                 © 2024 Aixploria. All rights reserved.
               </span>
               <Image
-                className='ml-2'
-                src='/aixploria-eco-friendly-green.webp'
-                alt='Green World'
-                title='Eco-Friendly'
+                className="ml-2"
+                src="/aixploria-eco-friendly-green.webp"
+                alt="Green World"
+                title="Eco-Friendly"
                 width={15}
                 height={15}
-                loading='lazy'
-                decoding='async'
+                loading="lazy"
+                decoding="async"
               />
               {/* <div className='flex justify-center items-center space-x-4 mt-2'> */}
               <a
-                href='#'
-                className='hover:text-gray-300 text-[12px] ml-2'
-                rel='nofollow noopener'
+                href="#"
+                className="hover:text-gray-300 text-[12px] ml-2"
+                rel="nofollow noopener"
               >
                 Cookie Policy
               </a>
               <a
-                href='#'
-                className='hover:text-gray-300 text-[12px] ml-2'
-                rel='nofollow noopener'
+                href="#"
+                className="hover:text-gray-300 text-[12px] ml-2"
+                rel="nofollow noopener"
               >
                 Conditions of use
               </a>
               <a
-                href='#'
-                className='hover:text-gray-300 text-[12px] ml-2'
-                rel='nofollow noopener'
+                href="#"
+                className="hover:text-gray-300 text-[12px] ml-2"
+                rel="nofollow noopener"
               >
                 Legals informations
               </a>
               {/* </div> */}
             </div>
-            <div className='flex items-center space-x-4 mb-4 md:mb-0'>
+            <div className="flex items-center space-x-4 mb-4 md:mb-0">
               <a
-                href='#'
-                target='_blank'
-                title='Aixploria X'
-                rel='noopener noreferrer'
+                href="#"
+                target="_blank"
+                title="Aixploria X"
+                rel="noopener noreferrer"
               >
                 <Image
-                  src='/x-icon-ai-style.webp'
-                  alt='Aixploria X'
+                  src="/x-icon-ai-style.webp"
+                  alt="Aixploria X"
                   width={20}
                   height={20}
-                  className='invert filter'
-                  loading='lazy'
-                  decoding='async'
+                  className="invert filter"
+                  loading="lazy"
+                  decoding="async"
                 />
               </a>
             </div>
@@ -214,7 +216,7 @@ function Footer() {
         </div>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,57 +1,57 @@
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import '../styles/style.css'
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import "../styles/style.css";
 import {
   Dropdown,
   DropdownTrigger,
   DropdownMenu,
   DropdownSection,
   Button,
-  DropdownItem
-} from '@nextui-org/react'
+  DropdownItem,
+} from "@nextui-org/react";
 export const Header = () => {
-  const [darkMode, setDarkMode] = useState(false)
-  const [menuDisplay, setMenuDisplay] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
+  const [menuDisplay, setMenuDisplay] = useState(false);
   const toggleDarkMode = () => {
-    setDarkMode(!darkMode)
-  }
+    setDarkMode(!darkMode);
+  };
 
   // Apply dark mode class to the <html> element
   useEffect(() => {
     if (darkMode) {
-      document.documentElement.classList.add('dark')
+      document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.remove('dark')
+      document.documentElement.classList.remove("dark");
     }
-  }, [darkMode])
+  }, [darkMode]);
 
   return (
-    <header id='header' className='site-header dark:bg-gray-800 w-full'>
-      <div className='container mx-auto flex flex-col sm:flex-row items-center justify-between py-4 px-8'>
+    <header id="header" className="site-header dark:bg-gray-800 w-full">
+      <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between py-4 px-8 ">
         {/* Left: Logo/Image */}
-        <Link href='/' className='flex items-center cursor-pointer'>
-          <span className='text-base sm:text-lg md:text-xl lg:text-2xl font-bold text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400'>
+        <Link href="/" className="flex items-center cursor-pointer">
+          <span className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold text-white dark:text-gray-300 hover:text-gray-300 dark:hover:text-blue-400">
             AI XPORIA
           </span>
         </Link>
 
         {/* Right: Navigation */}
-        <nav className='flex flex-wrap justify-around sm:justify-start space-x-2 sm:space-x-6 mt-4 sm:mt-0'>
+        <nav className="flex flex-wrap justify-around sm:justify-start space-x-2 sm:space-x-6 mt-4 sm:mt-0">
           <Link
-            href='/full-list'
-            className='text-sm sm:text-base md:text-lg lg:text-xl text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400'
+            href="/full-list"
+            className="text-sm sm:text-base md:text-lg lg:text-xl text-white dark:text-gray-300 hover:text-gray-300 dark:hover:text-blue-400"
           >
             Full List
           </Link>
           <Link
-            href='/ai-categories'
-            className='text-sm sm:text-base md:text-lg lg:text-xl text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400'
+            href="/ai-categories"
+            className="text-sm sm:text-base md:text-lg lg:text-xl text-white dark:text-gray-300 hover:text-gray-300 dark:hover:text-blue-400"
           >
             AI Categories
           </Link>
           <Link
-            href='/ai-tutorials'
-            className='text-sm sm:text-base md:text-lg lg:text-xl text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400'
+            href="/ai-tutorials"
+            className="text-sm sm:text-base md:text-lg lg:text-xl text-white dark:text-gray-300 hover:text-gray-300 dark:hover:text-blue-400"
           >
             AI Tutorials
           </Link>
@@ -62,7 +62,7 @@ export const Header = () => {
           >
             <DropdownTrigger>
               <div
-                className='text-sm sm:text-base md:text-lg lg:text-xl text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 cursor-pointer'
+                className="text-sm sm:text-base md:text-lg lg:text-xl text-white dark:text-gray-300 hover:text-gray-300 dark:hover:text-blue-400 cursor-pointer"
                 onMouseEnter={() => setMenuDisplay(true)}
                 onClick={() => setMenuDisplay(!menuDisplay)}
               >
@@ -70,71 +70,78 @@ export const Header = () => {
               </div>
             </DropdownTrigger>
             <DropdownMenu
-              aria-label='Static Actions'
+              className="bg-[#222222]"
+              aria-label="Static Actions"
               itemClasses={{
                 base: [
-                  'data-[hover=true]:text-foreground',
-                  'data-[hover=true]:bg-transparent',
-                  'dark:data-[hover=true]:bg-gray-500'
-                ]
+                  "data-[hover=true]:text-foreground",
+                  // "data-[hover=true]:bg-transparent",
+                  "dark:data-[hover=true]:bg-gray-500",
+                ],
               }}
             >
-              <DropdownItem key='news' className='bg-white hover:bg-white'>
+              <DropdownItem
+                key="news"
+                className="bg-[#222222] hover:bg-gray-600"
+              >
                 <Link
-                  href='/ai-news'
-                  className='flex px-4 py-2 text-sm sm:text-base md:text-lg lg:text-xl text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400'
+                  href="/ai-news"
+                  className="flex px-4 py-2 text-sm sm:text-base md:text-lg lg:text-lg text-white dark:text-gray-300 hover:text-black dark:hover:text-blue-400"
                 >
                   AI News
                 </Link>
               </DropdownItem>
-              <DropdownItem key='submit-ai' className='bg-white hover:bg-white'>
+              <DropdownItem
+                key="submit-ai"
+                className="bg-[#222222] hover:bg-gray-600"
+              >
                 <Link
-                  href='/submit-ai'
-                  className='flex px-4 py-2 text-sm sm:text-base md:text-lg lg:text-xl text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400'
+                  href="/submit-ai"
+                  className="flex px-4 py-2 text-sm sm:text-base md:text-lg lg:text-lg text-white dark:text-gray-300 hover:text-black dark:hover:text-blue-400"
                 >
                   Submit AI
                 </Link>
               </DropdownItem>
             </DropdownMenu>
           </Dropdown>
-          <button
-            onClick={toggleDarkMode}
-            className='text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 focus:outline-none'
-          >
-            {darkMode ? (
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-6 w-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M12 3v1m0 16v1m8.485-9.485h-1M4.515 12h-1m15.364-6.364l-.707-.707M6.343 17.657l-.707-.707m12.728 0l-.707.707M6.343 6.343l-.707.707M12 5a7 7 0 00-7 7 7 7 0 0014 0 7 7 0 00-7-7z'
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-6 w-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M12 3c4.971 0 9 4.029 9 9s-4.029 9-9 9-9-4.029-9-9 4.029-9 9-9z'
-                />
-              </svg>
-            )}
-          </button>
+          {/* <button
+              onClick={toggleDarkMode}
+              className="text-white dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 focus:outline-none"
+            >
+              {darkMode ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 3v1m0 16v1m8.485-9.485h-1M4.515 12h-1m15.364-6.364l-.707-.707M6.343 17.657l-.707-.707m12.728 0l-.707.707M6.343 6.343l-.707.707M12 5a7 7 0 00-7 7 7 7 0 0014 0 7 7 0 00-7-7z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 3c4.971 0 9 4.029 9 9s-4.029 9-9 9-9-4.029-9-9 4.029-9 9-9z"
+                  />
+                </svg>
+              )}
+            </button> */}
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
