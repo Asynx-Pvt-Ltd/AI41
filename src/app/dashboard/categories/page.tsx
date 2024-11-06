71d13a0657e06adbe6822c6ee6d2c99e65c6390a
Shadowctrl <shadowctrl@outlook.com>
2024-11-07 02:45:34 +0530

    "Updated Categories page to include font icon field, refactored code formatting and added error handling for API requests."

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,143 +1,160 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import DashboardLayout from '@/app/components/DashboardLayout'
-import { toast } from 'react-toastify'
-export const dynamic = 'force-dynamic'
-function Categories() {
-  const [categories, setCategories] = useState<any>([])
-  const [formData, setFormData] = useState<{ name: string }>({
-    name: ''
-  })
+"use client";
+import React, { useEffect, useState } from "react";
+import DashboardLayout from "@/app/components/DashboardLayout";
+import { toast } from "react-toastify";
+export const dynamic = "force-dynamic";
 
-  const [editMode, setEditMode] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const [submitting, setSubmitting] = useState(false)
-  const [editingCategory, setEditingCategory] = useState<any>(null)
+function Categories() {
+  const [categories, setCategories] = useState<any>([]);
+  const [formData, setFormData] = useState<{ name: string; fontIcon: string }>({
+    name: "",
+    fontIcon: "",
+  });
+
+  const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+  const [editingCategory, setEditingCategory] = useState<any>(null);
 
   const fetchCategories = async () => {
-    setLoading(true)
-    const response = await fetch('/api/categories')
-    const data = await response.json()
-    setCategories(data)
-    setLoading(false)
-  }
-  // Fetch tools from the backend
+    setLoading(true);
+    const response = await fetch("/api/categories");
+    const data = await response.json();
+    setCategories(data);
+    setLoading(false);
+  };
+
   useEffect(() => {
-    fetchCategories()
-  }, [])
+    fetchCategories();
+  }, []);
 
   const handleInputChange = (e: { target: { name: any; value: any } }) => {
-    const { name, value } = e.target
-    setFormData({ ...formData, [name]: value })
-  }
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   const handleFormSubmit = async (e: { preventDefault: () => void }) => {
-    e.preventDefault()
-    if (formData.name === '') {
-      toast.error('Add name')
-      return
+    e.preventDefault();
+    if (formData.name === "") {
+      toast.error("Add name");
+      return;
     }
-    setSubmitting(true)
+    if (formData.fontIcon === "") {
+      toast.error("Add font icon class");
+      return;
+    }
+    setSubmitting(true);
     var slug = formData.name
-      .split(' ')
+      .split(" ")
       .map((s) => s.toLowerCase())
-      .join('-')
-    if (editMode && editingCategory?.id) {
-      // Update tool
-      await fetch(`/api/categories/${editingCategory?.id}`, {
-        method: 'PUT',
-        body: JSON.stringify({ name: formData.name, slug: slug })
-      })
-        .then((res) => {
-          setSubmitting(false)
+      .join("-");
 
-          console.log('====================================')
-          console.log('res -->', res)
-          console.log('====================================')
-        })
-        .catch((err) => {
-          console.log('====================================')
-          console.log('err updating --->', err)
-          console.log('====================================')
-        })
-    } else {
-      // Create new tool
-      await fetch('/api/categories', {
-        method: 'POST',
-        body: JSON.stringify({ name: formData.name, slug: slug })
+    if (editMode && editingCategory?.id) {
+      // Update category
+      await fetch(`/api/categories/${editingCategory?.id}`, {
+        method: "PUT",
+        body: JSON.stringify({
+          name: formData.name,
+          slug: slug,
+          fontIcon: formData.fontIcon,
+        }),
       })
         .then((res) => {
-          console.log('====================================')
-          console.log('res --->', res)
-          console.log('====================================')
-          setSubmitting(false)
+          setSubmitting(false);
+          toast.success("Category updated successfully");
         })
         .catch((err) => {
-          console.log('====================================')
-          console.log('err posting --->', err)
-          console.log('====================================')
+          console.error("Error updating category:", err);
+          toast.error("Error updating category");
+          setSubmitting(false);
+        });
+    } else {
+      // Create new category
+      await fetch("/api/categories", {
+        method: "POST",
+        body: JSON.stringify({
+          name: formData.name,
+          slug: slug,
+          fontIcon: formData.fontIcon,
+        }),
+      })
+        .then((res) => {
+          setSubmitting(false);
+          toast.success("Category created successfully");
         })
+        .catch((err) => {
+          console.error("Error creating category:", err);
+          toast.error("Error creating category");
+          setSubmitting(false);
+        });
     }
-    // Refresh tools list
+
     setFormData({
-      name: ''
-    })
-    setEditMode(false)
-    fetchCategories()
-  }
+      name: "",
+      fontIcon: "",
+    });
+    setEditMode(false);
+    fetchCategories();
+  };
 
   const handleEdit = (category: any) => {
-    setEditMode(true)
-    setEditingCategory(category)
-    setFormData(category)
-  }
+    setEditMode(true);
+    setEditingCategory(category);
+    setFormData(category);
+  };
 
   const handleDelete = async (id: any) => {
-    setLoading(true)
+    setLoading(true);
     fetch(`/api/categories/${id}`, {
-      method: 'DELETE'
+      method: "DELETE",
     })
       .then((res) => res.json())
       .then((d) => {
-        setCategories(d)
-        setLoading(false)
+        setCategories(d);
+        setLoading(false);
+        toast.success("Category deleted successfully");
       })
       .catch((err) => {
-        console.log('====================================')
-        console.log('err ---->', err)
-        console.log('====================================')
-      })
-  }
+        console.error("Error deleting category:", err);
+        toast.error("Error deleting category");
+        setLoading(false);
+      });
+  };
 
   return (
     <DashboardLayout>
       <div>
-        <h1 className='text-2xl font-bold mb-4'>Categories</h1>
+        <h1 className="text-2xl font-bold mb-4">Categories</h1>
         <>
-          {/* Tool List Table */}
+          {/* Category List Table */}
           {loading === false ? (
-            <table className='min-w-full bg-white'>
+            <table className="min-w-full bg-white">
               <thead>
                 <tr>
-                  <th className='py-2 px-4'>Name</th>
+                  <th className="py-2 px-4">Name</th>
+                  <th className="py-2 px-4">Font Icon</th>
+                  <th className="py-2 px-4">Actions</th>
                 </tr>
               </thead>
               <tbody>
                 {categories.length
                   ? categories.map((category: any) => (
                       <tr key={category.id}>
-                        <td className='border px-4 py-2'>{category.name}</td>
-
-                        <td className='h-auto align-middle justify-center border px-4 py-2'>
-                          <div className='flex flex-row justify-center'>
+                        <td className="border px-4 py-2">{category.name}</td>
+                        <td className="border px-4 py-2">
+                          <i className={category.fontIcon}></i>
+                          <span className="ml-2">{category.fontIcon}</span>
+                        </td>
+                        <td className="h-auto align-middle justify-center border px-4 py-2">
+                          <div className="flex flex-row justify-center">
                             <button
-                              className='bg-blue-500 text-white px-4 py-3 mr-2'
+                              className="bg-blue-500 text-white px-4 py-3 mr-2"
                               onClick={() => handleEdit(category)}
                             >
                               Edit
                             </button>
                             <button
-                              className='bg-red-500 text-white px-4 py-2'
+                              className="bg-red-500 text-white px-4 py-2"
                               onClick={() => handleDelete(category.id)}
                             >
                               Delete
@@ -150,44 +167,58 @@ function Categories() {
               </tbody>
             </table>
           ) : (
-            <div className='flex justify-center items-center'>
-              <div className='animate-spin rounded-full h-16 w-16 border-t-4 border-black dark:border-white'></div>
+            <div className="flex justify-center items-center">
+              <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-black dark:border-white"></div>
             </div>
           )}
         </>
-        <h2 className='text-xl font-bold mt-4'>Add Tool</h2>
+        <h2 className="text-xl font-bold mt-4">
+          {editMode ? "Edit Category" : "Add Category"}
+        </h2>
         {/* Add/Edit Form */}
-        <form onSubmit={handleFormSubmit} className='flex flex-col mt-8'>
+        <form onSubmit={handleFormSubmit} className="flex flex-col mt-8">
           {submitting === false ? (
             <>
               <label>
-                Name <span className='text-red-500'>*</span>
+                Name <span className="text-red-500">*</span>
               </label>
               <input
-                type='text'
-                name='name'
-                placeholder='Name'
+                type="text"
+                name="name"
+                placeholder="Name"
                 value={formData.name}
                 onChange={handleInputChange}
-                className='max-w-52 block mb-4 p-2 border'
+                className="max-w-52 block mb-4 p-2 border"
+              />
+
+              <label>
+                Font Awesome class <span className="text-red-500">*</span>
+              </label>
+              <input
+                type="text"
+                name="fontIcon"
+                placeholder="e.g. fa-solid fa-user"
+                value={formData.fontIcon}
+                onChange={handleInputChange}
+                className="max-w-52 block mb-4 p-2 border"
               />
 
               <button
-                type='submit'
-                className='max-w-48 mt-4 bg-green-500 text-white px-4 py-2'
+                type="submit"
+                className="max-w-48 mt-4 bg-green-500 text-white px-4 py-2"
               >
-                {editMode ? 'Update Category' : 'Add Category'}
+                {editMode ? "Update Category" : "Add Category"}
               </button>
             </>
           ) : (
-            <div className='flex flex-row justify-center items-center'>
-              <div className='animate-spin rounded-full h-16 w-16 border-t-4 border-black dark:border-white'></div>
+            <div className="flex flex-row justify-center items-center">
+              <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-black dark:border-white"></div>
             </div>
           )}
         </form>
       </div>
     </DashboardLayout>
-  )
+  );
 }
 
-export default Categories
+export default Categories;
--- src/pages/api/categories/index.ts
+++ src/pages/api/categories/index.ts
@@ -1,49 +1,43 @@
-import { PrismaClient } from '@prisma/client'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'GET') {
-    const { category } = req.query
-    console.log('====================================')
-    console.log('category --->', category)
-    console.log('====================================')
+  if (req.method === "GET") {
+    const { category } = req.query;
+
     if (category) {
       const categories = await prisma.category.findUnique({
         where: {
-          slug: typeof category === 'object' ? category.join('') : category
+          slug: typeof category === "object" ? category.join("") : category,
         },
         include: {
-          tools: true
-        }
-      })
-      console.log('====================================')
-      console.log('categories --->', categories)
-      console.log('====================================')
-      return res.status(200).json(categories)
+          tools: true,
+        },
+      });
+
+      return res.status(200).json(categories);
     }
     const categories = await prisma.category.findMany({
       include: {
-        tools: true
-      }
-    })
-    return res.status(200).json(categories)
+        tools: true,
+      },
+    });
+    return res.status(200).json(categories);
   }
 
-  if (req.method === 'POST') {
-    const { name, slug } = JSON.parse(req.body)
-    console.log('====================================')
-    console.log('name --->', name)
-    console.log('====================================')
+  if (req.method === "POST") {
+    const { name, slug, fontIcon } = JSON.parse(req.body);
     const newCategory = await prisma.category.create({
       data: {
         name: name,
-        slug: slug
-      }
-    })
-    return res.status(201).json(newCategory)
+        slug: slug,
+        fontIcon: fontIcon,
+      },
+    });
+    return res.status(201).json(newCategory);
   }
 }
