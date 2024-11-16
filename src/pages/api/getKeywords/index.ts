ce2a257bec7702bb1cc8d667ef80a7176c3d7116
Shadowctrl <shadowctrl@outlook.com>
2024-11-16 19:16:59 +0530

    Added null check for `keywordEntry` and `keywordEntry.keyword` before returning response in `getKeywords` API handler.

--- src/pages/api/getKeywords/index.ts
+++ src/pages/api/getKeywords/index.ts
@@ -4,6 +4,11 @@ import prisma from "@/lib/primsa";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     const keywordEntry = await prisma.tutorialKeyword.findFirst();
+    if (!keywordEntry || !keywordEntry.keyword)
+      return res.status(400).json({
+        success: false,
+        message: "No keyword found in the database.",
+      });
     return res.status(200).json(keywordEntry.keyword);
   }
 };
