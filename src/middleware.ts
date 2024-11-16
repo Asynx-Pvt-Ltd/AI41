e195fdae0b0708c90f0dd65b62e7a86e9280e07d
Shadowctrl <shadowctrl@outlook.com>
2024-11-16 19:26:33 +0530

    "Added 'toolsdirectory-phi.vercel.app' to allowedOrigins in middleware"

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const middleware = (req: NextRequest) => {
-  const allowedOrigins = ["localhost:3000"];
+  const allowedOrigins = ["localhost:3000", "toolsdirectory-phi.vercel.app"];
   const origin = req.headers.get("x-forwarded-host") ?? "";
   const isAllowedOrigin = allowedOrigins.includes(origin);
 
