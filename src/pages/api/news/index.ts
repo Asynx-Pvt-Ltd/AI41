d0c932859ccf1c99bbefc96447b120fde25c5de0
Shadowctrl <shadowctrl@outlook.com>
2024-11-18 23:39:13 +0530

    "Added 'description' and 'slugUrl' fields to news creation, updated method checks to use double quotes"

--- src/pages/api/news/index.ts
+++ src/pages/api/news/index.ts
@@ -1,23 +1,25 @@
-import prisma from '../../../lib/primsa'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from "../../../lib/primsa";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
-    const { title, url, icon } = JSON.parse(req.body)
+  if (req.method === "POST") {
+    const { title, url, icon, description } = JSON.parse(req.body);
 
     const newTutorial = await prisma.news.create({
       data: {
         title: title,
         url: url,
-        icon: icon
-      }
-    })
-    return res.json(newTutorial)
-  } else if (req.method === 'GET') {
-    const tools = await prisma.news.findMany()
-    return res.json(tools)
+        icon: icon,
+        slugUrl: encodeURIComponent(title.replaceAll(" ", "-")),
+        description: description,
+      },
+    });
+    return res.json(newTutorial);
+  } else if (req.method === "GET") {
+    const tools = await prisma.news.findMany();
+    return res.json(tools);
   }
 }
