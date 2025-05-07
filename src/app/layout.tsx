8b05559f30a0e1fe6ad9ac2d5bbc9043c4ea59a9
shadowctrl <admin@shadowctrl.me>
2025-05-07 09:20:23 +0530

    Refactor SEO component and layout: switch to react-helmet-async for improved SEO handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
 import ClientSideToastContainer from './components/ClientSideToasterContainer';
 import Script from 'next/script';
+import { HelmetProvider } from 'react-helmet-async';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,8 +20,10 @@ export default function RootLayout({
 				<Script src="https://cdn.lordicon.com/lordicon.js" />
 			</head>
 			<body className={inter.className}>
-				<ClientSideToastContainer />
-				<Providers>{children}</Providers>
+				<HelmetProvider>
+					<ClientSideToastContainer />
+					<Providers>{children}</Providers>
+				</HelmetProvider>
 			</body>
 		</html>
 	);
